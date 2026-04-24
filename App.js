5e4a4cd3d13e079809ee2ef376cb697796175de6
fdelima-dev <fdelima.dev@gmail.com>
2026-04-24 13:15:56 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from "react-native"
+import { useState } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  const [nota1, setNota1] = useState('');
+  const [nota2, setNota2] = useState('');
+  const [nota3, setNota3] = useState('');
+  const [resultado, setResultado] = useState(null);
+
+  function calculaMedia(){
+    let n1 = parseFloat(nota1)
+    let n2 = parseFloat(nota2)
+    let n3 = parseFloat(nota3)
+
+    if (isNaN(n1) || isNaN(n2) || isNaN(n3)){
+      setResultado("Insira dados válidos")
+      return;
+    }
+
+    setResultado((n1 + n2 + n3) / 3);
+  }
+  return(
+    <View>
+      <Text>Digite as notas ∛</Text>
+      <TextInput style={styles.input} onChangeText={setNota1} keyboardType="numeric" />
+      <TextInput style={styles.input} onChangeText={setNota2} keyboardType="numeric" />
+      <TextInput style={styles.input} onChangeText={setNota3} keyboardType="numeric" />
+      <Button onPress={calculaMedia} title="Calcular" />
+      <View>
+        {
+          resultado != null && resultado > 7  && (
+            <Text>Parabéns, sua média é {resultado.toFixed(1)} e você está aprovado!</Text>
+          )
+        }
+        {
+          resultado != null && resultado < 7 && (
+            <Text>Que pena, sua média foi {resultado.toFixed(1)} e você está reprovado</Text>
+          )
+        }
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  input: {
+    borderWidth: 1,
+    borderColor: '#007bff',
+    marginBottom: 10,
+  }
+})
